36a82f93d3a62e09cda33b2c59f442908a854215
CrisAr1008 <craparrag@correo.udistrital.edu.co>
2024-08-26 21:13:33 -0500

    Conexion y DAO

--- src/configuration/Conexion.java
+++ src/configuration/Conexion.java
@@ -3,15 +3,24 @@ package configuration;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.io.File;
 
 /**
  * Clase Conexion para manejar la conexión con una base de datos SQLite.
+ * La base de datos se almacenará en la carpeta raíz del proyecto.
  * @author cparra
  */
 public class Conexion {
 
-    private static final String URL = "jdbc:sqlite:C:\\Users\\cparra\\Documents\\NetBeansProjects\\CalidadSoftware\\BaseDatosSQLite\\BD.db";
+    private static final String DB_NAME = "BD.db";
+    private static final String URL = "jdbc:sqlite:" + getDatabasePath();
     private Connection conexion = null;
+
+    // Método para obtener la ruta del archivo de la base de datos en la carpeta raíz del proyecto
+    private static String getDatabasePath() {
+        String projectRoot = new File("").getAbsolutePath();  // Obtiene la ruta absoluta del proyecto
+        return projectRoot + File.separator + DB_NAME;  // Concatena la ruta del proyecto con el nombre de la base de datos
+    }
 
     // Método para abrir la conexión
     public Connection conectar() {
